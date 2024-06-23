c3dc64580121560e7868562bac39ad654298cf65
Latihan Git <melonpea12@gmail.com>
2024-06-23 17:32:16 +0700

    ubah home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,11 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Home') }}
-        </h2>
-    </x-slot>
-    <!-- Hero Section -->
-    <section class="bg-cover bg-center h-screen relative">
-        <div class="bg-cover bg-center h-screen relative" style="background-image: url('{{ asset('img/banyak_mobil.png') }}')">
-            <h1 class="text-4xl md:text-6xl font-bold mb-4">SewainAjA Hadirkan Customer Portal yang Praktis dan Inovatif!</h1>
-            <a href="#" class="mt-4 bg-white hover:bg-orange-700 text-orange-500 hover:text-white font-bold py-3 px-6 rounded-full">www.mpm-rent.com</a>
-        </div>
-        <!-- Search Box -->
-        <div class="absolute bottom-0 left-0 right-0 bg-white py-6 shadow-lg">
-            <div class="container mx-auto px-4">
-                <div class="bg-white p-6 rounded-lg shadow-lg">
-                    <div class="flex justify-center space-x-4 mb-4">
-                        <button class="bg-orange-500 text-white py-2 px-4 rounded-full">Pribadi</button>
-                        <button class="text-orange-500 border border-orange-500 py-2 px-4 rounded-full">Korporasi</button>
+<x-app-layout>     <!-- Hero Section -->
+    <section class="p-10"> <!-- Menyesuaikan padding atas dan bawah -->
+        <div class="bg-white flex flex-col text-black p-16">
+            <div class="container mx-auto px-4 md:px-0"> <!-- Menyesuaikan padding kiri kanan dan mengatur lebar konten -->
+                <div class="flex flex-col md:flex-row items-center md:items-start">
+                    <div class="md:w-1/2">
+                        <h1 class="md:text-3xl font-bold mb-4">SewainAjA! Rental mobil kesukaan kamu disini</h1>
+</h1>
                     </div>
                     <div class="md:w-1/2 flex justify-center md:justify-end">
                         <img src="{{ asset('img/mobil-rent.jpg') }}" class="w-96 md:max-w-xl mx-auto md:mx-0"> <!-- Menyesuaikan lebar gambar dengan ukuran maksimum pada desktop -->
--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,6 @@ use App\Http\Controllers\OwnerController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
     return view('home');
 });
 
