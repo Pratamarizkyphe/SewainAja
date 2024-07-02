483143016de40de28890d6a56bd32733c0718444
Latihan Git <melonpea12@gmail.com>
2024-07-02 23:36:57 +0700

    fix

--- resources/views/user/riwayat-penyewaan.blade.php
+++ resources/views/user/riwayat-penyewaan.blade.php
@@ -47,10 +47,6 @@
                                             <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
                                         </svg>
                                     </a>
-                                    <a class=" text-red-500 ml-2" href=""><svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
-                                    </svg>
-                                    </a>
                                     @if ($penyewaan->status_pembayaran == 'Belum Dibayar')
                                         <a href="{{ route('showPaymentForm') }}" class="text-green-500 ml-2">Bayar</a>
                                     @endif
